c1011dbcbf5ffaa54ceea637aff2f5e678d695e1
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-06-27 22:46:13 +0700

    feat: add upload image api

---
+++ src/routes/image.js
@@ -0,0 +1,9 @@
+module.exports = (app) => {
+  const router = require("express").Router();
+  const imageController = require("../controllers/images.js");
+
+  // Routes
+  router.get("/", imageController.upload);
+
+  app.use("/api/v1/images", router);
+};
