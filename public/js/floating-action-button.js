5563fb62ff78d456a5582a019b61f3eee124c7c5
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-07-24 15:13:44 +0700

    feat: add fontawesome libs

--- public/js/floating-action-button.js
+++ public/js/floating-action-button.js
@@ -4,12 +4,44 @@ const generateButton = async (id) => {
   const response = await fetch(
     "http://localhost:8080/api/v1/user/my-products/" + id + "/public-button"
   );
-  const data = await response.json();
+  const style = await response.json();
+  console.log(style);
 
-  console.log(data);
+  const divCss = {
+    position: "absolute",
+    top: style.data.top ? style.data.top : null,
+    right: style.data.right ? style.data.right : null,
+    bottom: style.data.bottom ? style.data.bottom : null,
+    left: style.data.left ? style.data.left : null,
+  };
 
+  const buttonCss = {
+    background: style.data.backgroundColor,
+    border: "none",
+    color: style.data.textColor,
+    width: style.data.size,
+    height: style.data.size,
+    borderRadius: "50%",
+    fontSize: 32,
+  };
+
+  const splitIcon = style.data.icon.split(" ");
+
+  let prefixIcon;
+
+  if (splitIcon[0] === "fas") {
+    prefixIcon = "fa-solid";
+  } else {
+    prefixIcon = "fa-brands";
+  }
+
+  let iconValue = "fa-" + splitIcon[1];
+
+  const div = document.createElement("div");
   const button = document.createElement("button");
-  button.innerHTML = "Test";
-  console.log(button);
-  document.body.appendChild(button);
+  button.innerHTML = `<i class="${prefixIcon} ${iconValue}"></i>`;
+  div.appendChild(button);
+  document.body.appendChild(div);
+  Object.assign(button.style, buttonCss);
+  Object.assign(div.style, divCss);
 };
