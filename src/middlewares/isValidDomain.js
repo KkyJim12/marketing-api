2ab88fb8c17e4720c77790a0ee4dbbc4b2d9a347
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-08-17 00:58:29 +0700

    feat: add log to watch error

--- src/middlewares/isValidDomain.js
+++ src/middlewares/isValidDomain.js
@@ -2,13 +2,17 @@ const db = require("../models/index");
 const WhiteListDomain = db.whiteListDomain;
 
 module.exports = isValidDomain = async (req, res, next) => {
-  const domainCount = await WhiteListDomain.count({
-    where: { userProductId: req.params.id, url: req.headers.requesthost },
-  });
-
-  if (domainCount > 0) {
-    return next();
-  } else {
-    res.status(422).send({ message: "This domain is not in whitelist." });
+  try {
+    const domainCount = await WhiteListDomain.count({
+      where: { userProductId: req.params.id, url: req.headers.requesthost },
+    });
+    console.log(domainCount);
+    if (domainCount > 0) {
+      return next();
+    } else {
+      res.status(422).send({ message: "This domain is not in whitelist." });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
