b831a469b0acd7c11495a56771411b413b92ec06
bewza0210 <piyapatnakonwan@gmail.com>
2024-11-21 00:26:21 +0700

    แก้ bug ในส่วนของ middleware ที่ต้อง return ทุกครั้ง

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -10,16 +10,16 @@ module.exports = isAuth = async (req, res, next) => {
         process.env.ACCESS_TOKEN_SIGN_SECRET,
         (err, decoded) => {
           if (err) {
-            res.status(403).send({ message: "Please login first" });
+            return res.status(403).send({ message: "Please login first" });
           }
           req.user = decoded.user;
           return next();
         }
       );
     } else {
-      res.status(403).send({ message: "Please login first" });
+      return res.status(403).send({ message: "Please login first" });
     }
   } catch (error) {
-    res.status(500).send({ message: "Something went wrong " });
+    return res.status(500).send({ message: "Something went wrong " });
   }
 };
